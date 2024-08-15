6ce273c6eec7d0f2e5f0e549b9d3f041d00fd741
amaytu <gabrieljucarvalho@gmail.com>
2024-08-15 10:26:26 -0300

    soap

---
+++ SOAP/pokeAPISOAP/src/test/java/com/ldtt/pokesoap/PokesoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldtt.pokesoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokesoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
