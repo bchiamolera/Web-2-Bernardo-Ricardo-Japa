b99fa6dc8b2068472e4323c45345832d2dfdd94e
Ricardo Sasse <ricardosasse98@gmail.com>
2024-09-12 07:31:39 -0300

    Update TeamServer.java

--- SOAP/PokeSOAP/src/main/java/furb/web2024/pokesoap/TeamServer.java
+++ SOAP/PokeSOAP/src/main/java/furb/web2024/pokesoap/TeamServer.java
@@ -8,7 +8,9 @@ import javax.jws.soap.SOAPBinding.Style;
 @WebService
 @SOAPBinding(style = Style.RPC)
 public interface TeamServer {    
-        
+     
+    
+    //Servidor é o mesmo que um metodo mas para o cliente
     @WebMethod String getTeam();
     
     @WebMethod String createTeam(String trainerName, int[] pokemons);
